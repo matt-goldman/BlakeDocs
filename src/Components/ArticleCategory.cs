00f7580c3cad3c92605c6e596e572fc01f5c3631
Matt Goldman <matt.goldman@outlook.com>
2025-08-04 11:48:46 +1000

    Add ArticleCategory model and update Home.razor to manage categories

---
+++ src/Components/ArticleCategory.cs
@@ -0,0 +1,11 @@
+using Blazicons;
+
+namespace BlakeSampleDocs.Components;
+
+public record ArticleCategory(
+    SvgIcon? Icon,
+    string Title,
+    string? Description = null,
+    string? Link = null,
+    int ArticleCount = 0
+);
